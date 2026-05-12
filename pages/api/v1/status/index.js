2a44e1b78c826ffa0c09ffc64618b006c7734495
Leonardo Elian de Carvalho de Almeida <82073486+Nnalie@users.noreply.github.com>
2026-05-12 02:18:47 +0000

    finish  endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,26 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result);
-  response.status(200).send("Alunos do curso.dev sao pessoas acima da media");
+  const updatedAt = new Date().toISOString();
+  const db_version = await database.query("SHOW server_version;");
+  const maxConn = await database.query("SHOW max_connections;");
+
+  const databaseName = 'local_db';
+  const openedConn = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datName=$1;",
+    values: [databaseName]
+  });
+
+  response.status(200).send({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: db_version.rows[0].server_version,
+        max_connections: parseInt(maxConn.rows[0].max_connections),
+        opened_connections: openedConn.rows[0].count
+      }
+    }
+  });
 }
 
 export default status;
